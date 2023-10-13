59e102467494acff93d25253f44d0195de9633b8
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-14 00:05:06 +0530

    added basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="font-righteous">
-      <h1>Helo world</h1>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Landing/>} exact/>
+          <Route path="/homepage" element={<HomePage/>} exact/>
+          <Route path="/profile" element={<Profile/>} exact/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+//@Aryan you can add ur code here :)
+
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>Landing</div>
+  )
+}
+
+export default Landing
