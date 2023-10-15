74c28193b1e8c806a354610fffb112d442bb0a3a
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-15 16:23:53 +0530

    document

--- src/pages/Documents.js
+++ src/pages/Documents.js
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import app from '../db/Firebase'; 
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { collection, addDoc, getDocs, deleteDoc, doc } from 'firebase/firestore';
 
 const Documents = () => {
-  return (
-    <div>Documents</div>
-  )
-}
+  const { user } = useAuth0();
+  const [documents, setDocuments] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
 
-export default Documents
+  const storage = getStorage(app);
+
+  useEffect(() => {
+    const fetchDocuments = async () => {
+      if (user) {
+        const documentsRef = collection(app.firestore(), `users/${user.sub}/documents`);
+        const querySnapshot = await getDocs(documentsRef);
+        const docs = [];
+
+        querySnapshot.forEach((doc) => {
+          docs.push({ id: doc.id, ...doc.data() });
+        });
+
+        setDocuments(docs);
+      }
+    };
+
+    fetchDocuments();
+  }, [user]);
+
+  const handleFileUpload = async () => {
+    if (user && selectedFile) {
+      const storageRef = ref(storage, `users/${user.sub}/${selectedFile.name}`);
+      await uploadBytes(storageRef, selectedFile);
+      const downloadURL = await getDownloadURL(storageRef);
+
+      const documentsRef = collection(app.firestore(), `users/${user.sub}/documents`);
+      const newDocument = {
+        name: selectedFile.name,
+        url: downloadURL,
+        timestamp: new Date(),
+      };
+
+      // Add the new document to the Firestore collection
+      const docRef = await addDoc(documentsRef, newDocument);
+
+      // Update the documents state to include the new document
+      setDocuments([...documents, { id: docRef.id, ...newDocument }]);
+    }
+  };
+
+  const handleFileSelect = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleDeleteDocument = async (documentId) => {
+    if (user) {
+      const documentsRef = collection(app.firestore(), `users/${user.sub}/documents`);
+      const documentRef = doc(documentsRef, documentId);
+
+      try {
+        const documentData = (await documentRef.get()).data();
+
+        if (documentData) {
+          const storageRef = ref(storage, documentData.url);
+          await deleteDoc(documentRef);
+          await deleteDoc(storageRef);
+
+          // Update the documents state by removing the deleted document
+          setDocuments(documents.filter((doc) => doc.id !== documentId));
+        } else {
+          console.error('Document does not exist.');
+        }
+      } catch (error) {
+        console.error('Error deleting document:', error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h2>Documents</h2>
+      <input type="file" onChange={handleFileSelect} />
+      <button onClick={handleFileUpload}>Upload Document</button>
+      <div className="documents-container">
+        {documents.map((document) => (
+          <div className="document" key={document.id}>
+            <a href={document.url} target="_blank" rel="noopener noreferrer">
+              {document.name}
+            </a>
+            <button onClick={() => handleDeleteDocument(document.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Documents;
