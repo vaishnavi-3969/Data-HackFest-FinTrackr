d620e01b2d5ea22ad67639648e5040244def4c13
kapooraryan <kapoor95aryan@gmail.com>
2023-10-15 12:50:25 +0530

    Added the FAQ section

---
+++ src/constants/faqs.js
@@ -0,0 +1,42 @@
+export const faqs = [
+  {
+    id: 1,
+    question: "Why do I need a Personal Finance Tracker?",
+    answer: "Managing personal finances is a fundamental aspect of an individual's life, and it can often become overwhelming to keep track of expenses, income, and financial goals. FinTrackr helps individuals take control of their finances, make informed financial decisions, and plan for their future effectively."
+  },
+  {
+    id: 2,
+    question: "How can I track my expenses and income with this application?",
+    answer: "You can easily record and categorize your expenses, income, and transactions with FinTrackr. It allows you to input your financial data and keep track of your financial activity in one place."
+  },
+  {
+    id: 3,
+    question: "Can I set and track my budget and financial goals with this tool?",
+    answer: "Yes, FinTrackr enables users to set budgets and financial goals, providing them with real-time progress updates."
+  },
+  {
+    id: 4,
+    question: "Does the Personal Finance Tracker integrate with my bank accounts?",
+    answer: "Yes, our application integrates with banking services (e.g., Plaid) to securely link and retrieve your bank account information, transaction history, and balances."
+  },
+  {
+    id: 5,
+    question: "How is my financial data protected in this application?",
+    answer: "We implement secure user authentication using Auth0 and Firebase to safeguard your financial data and ensure that only authorized users have access to their accounts."
+  },
+  {
+    id: 6,
+    question: "How is my financial data presented to me?",
+    answer: "We use charts and graphs to present financial data in an easily digestible format, helping users make informed decisions."
+  },
+  {
+    id: 7,
+    question: "How can I get started with the Personal Finance Tracker?",
+    answer: "To get started, you can sign up for an account, link your bank account/(s), and begin recording your financial transactions. The tool will guide you through the process to effectively manage your personal finances."
+  },
+  {
+    id: 8,
+    question: "How can I contact support if I encounter any issues or have queries?",
+    answer: "You can contact our support team through the contact information provided on our website, and they will be happy to assist you with any issues or queries you may have."
+  }
+];
